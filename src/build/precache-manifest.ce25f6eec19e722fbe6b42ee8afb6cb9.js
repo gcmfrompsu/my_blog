49ea0e62ac63328e5d8a6da5d7e2e8e38eff6f03
gcmfrompsu <gcmfrompsu@gmail.com>
2019-07-03 02:04:39 -0400

    First commit

---
+++ src/build/precache-manifest.ce25f6eec19e722fbe6b42ee8afb6cb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db2ebe66b89b9c889a9fb308b2fdb829",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c349eb442d489b5d4664",
+    "url": "/static/css/main.fe6824ef.chunk.css"
+  },
+  {
+    "revision": "3ee9e378dc01ead8ca45",
+    "url": "/static/js/2.f0d235d2.chunk.js"
+  },
+  {
+    "revision": "c349eb442d489b5d4664",
+    "url": "/static/js/main.4b4b281c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
